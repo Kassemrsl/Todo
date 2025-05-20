01bc4824e536deca3b4fdd7a604696ce2e76e887
Ali Darwish <Ali.Darwish@bottomline.com>
2025-05-20 23:42:45 +0300

    remove not used package

--- Todo.OnlineTaskManagement.Application/Services/AccountService.cs
+++ Todo.OnlineTaskManagement.Application/Services/AccountService.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNet.Identity.EntityFramework;
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
